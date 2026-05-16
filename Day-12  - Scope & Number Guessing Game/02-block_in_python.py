c9ab251c032bd025ecb8d0add5160613c8838a28
negaromanzir-lab <negaromanzir@gmail.com>
2026-05-16 21:02:30 +0300

    Understanding the block in the python programming that defferent from other programming languages

---
+++ Day-12  - Scope & Number Guessing Game/02-block_in_python.py
@@ -0,0 +1,11 @@
+#there is no block scope in python, only function scope. This means that variables defined inside a block (like an if statement or a for loop) are still accessible outside of that block. However, variables defined inside a function are not accessible outside of that function.
+
+game_level = 6 #global scope
+
+def create_enemy():
+    enemies = ["Skeleton", "Zombie", "Alien"] 
+    if game_level < 5:
+        new_enemy = enemies[0]
+    print(new_enemy)
+
+create_enemy()
