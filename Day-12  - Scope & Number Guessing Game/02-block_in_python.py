94e4052fa2c79a269e36d3cf021ae84f6b1c3c36
negaromanzir-lab <negaromanzir@gmail.com>
2026-05-16 21:11:51 +0300

    commenting

--- Day-12  - Scope & Number Guessing Game/02-block_in_python.py
+++ Day-12  - Scope & Number Guessing Game/02-block_in_python.py
@@ -1,4 +1,4 @@
-#there is no block scope in python, only function scope. This means that variables defined inside a block (like an if statement or a for loop) are still accessible outside of that block. However, variables defined inside a function are not accessible outside of that function.
+#There is no block scope in python, only function scope. This means that variables defined inside a block (like an if statement or a for loop) are still accessible outside of that block. However, variables defined inside a function are not accessible outside of that function.
 
 game_level = 6 #global scope
 
